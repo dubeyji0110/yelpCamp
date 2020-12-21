00a928b9a073fe89d876ad201ebb4a1d107c4779
Devansh Dubey <devanshdubey2001@gmail.com>
2020-12-21 16:38:46 +0530

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const campground = require('../models/campground');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('Database Connected!');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)]; 
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const rand = Math.floor(Math.random() * 200);
+        const price = Math.floor(Math.random() * 20 + 10); 
+        const camp = new Campground({
+            location: `${cities[rand].name}, ${cities[rand].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dolor aperiam ipsa officiis sequi est cum recusandae ex nobis explicabo? Magni quisquam recusandae deserunt quas eveniet hic nisi corrupti error!',
+            price
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
