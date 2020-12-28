0b126abb27ae6b14b6745bce4a7de4df13fa63b5
Devansh Dubey <devanshdubey2001@gmail.com>
2020-12-28 20:48:29 +0530

    uploading and deleting images add

--- Routes/campground.js
+++ Routes/campground.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const multer = require('multer');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campground = require('../controllers/campgrounds');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 const router = express.Router();
 
 router.route('/')
     .get(catchAsync(campground.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campground.createCampground))
-    
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campground.createCampground));
+
 router.get('/new', isLoggedIn, campground.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campground.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campground.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campground.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campground.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campground.renderEditForm));
--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,50 @@ const seedDB = async () => {
             author: '5fe752b80ef0ec1964549f0a',
             location: `${cities[rand].name}, ${cities[rand].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dolor aperiam ipsa officiis sequi est cum recusandae ex nobis explicabo? Magni quisquam recusandae deserunt quas eveniet hic nisi corrupti error!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162088/YelpCamp/ahfnenvca4tha00h2ubt_wwobds.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt_wwobds'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162092/YelpCamp/ruyoaxgf72nzpi4y6cdi_trycfn.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi_trycfn'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162691/YelpCamp/8_dhhrl0.jpg',
+                    filename: 'YelpCamp/8_dhhrl0'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162691/YelpCamp/5_bq7icw.jpg',
+                    filename: 'YelpCamp/5_bq7icw'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162691/YelpCamp/6_sjiqgp.jpg',
+                    filename: 'YelpCamp/6_sjiqgp'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162691/YelpCamp/4_bpkbsw.jpg',
+                    filename: 'YelpCamp/4_bpkbsw'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162690/YelpCamp/1_aojdp0.jpg',
+                    filename: 'YelpCamp/1_aojdp0'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162687/YelpCamp/7_w5qoas.jpg',
+                    filename: 'YelpCamp/7_w5qoas'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162687/YelpCamp/3_gbkvop.jpg',
+                    filename: 'YelpCamp/3_gbkvop'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dyekojods/image/upload/v1609162686/YelpCamp/2_tyme0x.jpg',
+                    filename: 'YelpCamp/2_tyme0x'
+                }
+            ]
         });
         await camp.save();
     }
