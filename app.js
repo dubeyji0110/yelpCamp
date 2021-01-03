02272f4d5ff303d48ddfffcbbf025609ad297739
Devansh Dubey <devanshdubey2001@gmail.com>
2021-01-03 19:00:26 +0530

    changed scripts

--- app.js
+++ app.js
@@ -154,6 +154,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('layouts/error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
